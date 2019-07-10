2362eb1b5140328b30307a9d9e11aa3c50126377
Stephen Ankoue <sankoue@codeustudents.com>
2019-07-10 18:58:37 -0400

    Fixed issues with homepage and nav bar

--- src/main/webapp/js/Navigation-bar/navigation-bar.js
+++ src/main/webapp/js/Navigation-bar/navigation-bar.js
@@ -3,17 +3,21 @@
 const e = React.createElement;
 
 class NavigationBar extends React.Component{
-  render() { 
+  render() {
     return (
       <nav>
       <ul id="navigation">
-        <li><a className="nav-link" href="/">
-        Home
-</a>
-</li>
-        <li><a className="nav-link" href="/aboutus.html">About Our Team</a></li>
+
+        <li><a className="nav-link" href="/">Home</a></li>
+        <li><a className="nav-link" href="/aboutus.html">Meet the Team</a></li>
+        <li><a className="nav-link" href="/community.html">Community</a></li>
+        <li><a className="nav-link" href="/stats.html">Stats</a></li>
+        <li><a className="nav-link" href="/feed.html">Orders</a></li>
+        <li><a className="nav-link" href="/restaurant-map.html">Map</a></li>
+        <li><a className="nav-link" href="/chart.html">Reviews</a></li>
+
       </ul>
-    </nav> 
+    </nav>
     );
   }
 }
