72427d9a891728962800f7c0335f0c91885348e2
TSykes21 <tiarasykes@Tiaras-MacBook-Pro.local>
2019-07-09 22:31:28 -0400

    Fixed the Redirect logic for claim button

--- src/main/webapp/js/Claim-Component/index.js
+++ src/main/webapp/js/Claim-Component/index.js
@@ -5,10 +5,28 @@ const e = React.createElement;
 class ClaimLink extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      hits: '',
+    }
   }
+
+  componentDidMount() {
+    fetch('/login-status')
+      .then(response => response.json())
+      .then((loginStatus) => {
+        if (loginStatus.isLoggedIn) {
+          this.setState({ hits: '/feed.html'}, () => {console.log("state updated:", this.state.hits)});
+        }
+        else {
+          this.setState({ hits: '/login'});
+        }
+      })   
+  }
+  
   render() {
     return(
-    <a href='/feed.html'>Claim Requests</a>);
+    <a href={this.state.hits}>Claim Requests</a>);
   }
 }
 
