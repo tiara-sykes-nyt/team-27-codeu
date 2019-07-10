3281c78a010d451dc02d175cc89f95cb748d50f9
Daniel Lee <danielylee01@gmail.com>
2019-07-10 11:06:26 -0400

    Fixed image display behavior on user-page.jsp

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -78,7 +78,7 @@ function showMessageFormIfViewingSelf() {
     headerDiv.classList.add('message-header');
     headerDiv.appendChild(document.createTextNode(
       message.user + ' - ' + new Date(message.timestamp)));
-      
+
       const bodyDiv = document.createElement('div');
       bodyDiv.classList.add('message-body');
       bodyDiv.innerHTML = message.text;
@@ -91,7 +91,8 @@ function showMessageFormIfViewingSelf() {
       messageDiv.classList.add('message-div');
       messageDiv.appendChild(headerDiv);
       messageDiv.appendChild(bodyDiv);
-      messageDiv.appendChild(imageDiv);
+      if (message.imageURL != null)
+        messageDiv.appendChild(imageDiv);
 
       return messageDiv;
     }
